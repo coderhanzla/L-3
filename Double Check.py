5078989e27a6f7e4be3aea0239b03bc8c3581d19
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-03 19:55:15 +0530

    Double Check

---
+++ Double Check.py
@@ -0,0 +1,12 @@
+number = int(input("Enter the Number to Check: "))
+
+if number > 50:
+    print("The Number Is Greater Than 50 ")
+
+    if number%2==0:
+              print("It is an Even number")
+
+    else:   
+              print("It is an Odd Number")
+else:
+    print("The Number is less than 50")
