52af39513aaaf4173e8fb83d3d3cf5a792088407
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-03 19:22:28 +0530

    Odd & Even

---
+++ Odd Even.py
@@ -0,0 +1,9 @@
+number = int(input("Enter the Number: "))
+
+print("The Number is to be checked ",number)
+
+if number%2==0:
+  print("It is an Even number")
+
+else:
+  print("It is an Odd Number")
